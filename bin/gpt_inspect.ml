30f4e878cf3a285dbb42139297d43b9158f216c2
PizieDust <playersrebirth@gmail.com>
2023-08-15 17:21:13 +0100

    update debug binary

--- bin/gpt_inspect.ml
+++ bin/gpt_inspect.ml
@@ -2,27 +2,6 @@ open Cmdliner
 
 let _sizeof = 128
 
-let print_gpt_header gpt =
-  Printf.printf "GPT header:\n";
-  Printf.printf "  signature: %s\n" gpt.Gpt.signature;
-  Printf.printf "  revision: 0x%08lx\n" gpt.Gpt.revision;
-  Printf.printf "  gpt-size: %ld\n" gpt.Gpt.header_size;
-  Printf.printf "  gpt-checksum: %ld\n" gpt.Gpt.header_crc32;
-  Printf.printf "  reserved: %ld\n" gpt.Gpt.reserved;
-  Printf.printf "  current-lba: %Ld\n" gpt.Gpt.current_lba;
-  Printf.printf "  backup-lba: %Ld\n" gpt.Gpt.backup_lba;
-  Printf.printf "  first-usable-lba: %Ld\n" gpt.Gpt.first_usable_lba;
-  Printf.printf "  last-usable-lba: %Ld\n" gpt.Gpt.last_usable_lba;
-  Printf.printf "  disk-guid: %s\n"
-    (Uuidm.to_string ~upper:true gpt.Gpt.disk_guid);
-  Printf.printf "  first-partition-lba: %Ld\n" gpt.Gpt.partition_entry_lba;
-  Printf.printf "  total-partitions: %d\n"
-    (Int32.to_int gpt.Gpt.num_partition_entries);
-  Printf.printf "  partition-size: %ld\n" gpt.Gpt.partition_size;
-  Printf.printf "  partitions-checksum: %ld\n" gpt.Gpt.partitions_crc32;
-  Printf.printf "  Partitions: \n";
-  Printf.printf "  -------------- \n"
-
 let print_gpt_partitions partitions =
   List.iteri
     (fun i part ->
@@ -42,6 +21,28 @@ let print_gpt_partitions partitions =
         Printf.printf "      ending-lba: %Ld\n" part.Gpt.Partition.ending_lba))
     partitions
 
+let print_gpt_header gpt =
+  Printf.printf "GPT header:\n";
+  Printf.printf "  signature: %s\n" gpt.Gpt.signature;
+  Printf.printf "  revision: 0x%08lx\n" gpt.Gpt.revision;
+  Printf.printf "  gpt-size: %ld\n" gpt.Gpt.header_size;
+  Printf.printf "  gpt-checksum: %ld\n" gpt.Gpt.header_crc32;
+  Printf.printf "  reserved: %ld\n" gpt.Gpt.reserved;
+  Printf.printf "  current-lba: %Ld\n" gpt.Gpt.current_lba;
+  Printf.printf "  backup-lba: %Ld\n" gpt.Gpt.backup_lba;
+  Printf.printf "  first-usable-lba: %Ld\n" gpt.Gpt.first_usable_lba;
+  Printf.printf "  last-usable-lba: %Ld\n" gpt.Gpt.last_usable_lba;
+  Printf.printf "  disk-guid: %s\n"
+    (Uuidm.to_string ~upper:true gpt.Gpt.disk_guid);
+  Printf.printf "  first-partition-lba: %Ld\n" gpt.Gpt.partition_entry_lba;
+  Printf.printf "  total-partitions: %d\n"
+    (Int32.to_int gpt.Gpt.num_partition_entries);
+  Printf.printf "  partition-size: %ld\n" gpt.Gpt.partition_size;
+  Printf.printf "  partitions-checksum: %ld\n" gpt.Gpt.partitions_crc32;
+  Printf.printf "  Partitions: \n"; 
+  Printf.printf "  *********** \n"; 
+  print_gpt_partitions gpt.Gpt.partitions
+
 let read_gpts gpts =
   List.iter
     (fun gpt ->
@@ -50,39 +51,11 @@ let read_gpts gpts =
       let () = seek_in ic 512 in
       let () = really_input ic buf 0 512 in
       close_in ic;
-      match Gpt.unmarshal (Cstruct.of_bytes buf) with
+      match Gpt.unmarshal (Cstruct.of_bytes buf) ~sector_size:512 with
       | Ok gpt ->
           print_gpt_header gpt;
-          let partition_buf =
-            Bytes.create
-              (Int32.to_int gpt.Gpt.num_partition_entries
-              * Int32.to_int gpt.Gpt.partition_size)
-          in
-          let partition_entry_offset =
-            Int64.to_int gpt.Gpt.partition_entry_lba * 512
-          in
-          let () = seek_in ic partition_entry_offset in
-          let () =
-            really_input ic partition_buf 0 (Bytes.length partition_buf)
-          in
-          close_in ic;
-          let partition_entries = ref [] in
-          for i = 0 to Int32.to_int gpt.Gpt.num_partition_entries - 1 do
-            let entry_offset = i * Int32.to_int gpt.Gpt.partition_size in
-            let entry_buf =
-              Cstruct.sub
-                (Cstruct.of_bytes partition_buf)
-                entry_offset
-                (Int32.to_int gpt.Gpt.partition_size)
-            in
-            match Gpt.Partition.unmarshal entry_buf with
-            | Ok entry -> partition_entries := entry :: !partition_entries
-            | Error e -> Printf.printf "An error occurred: %s\n" e
-          done;
-          let partitions = List.rev !partition_entries in
-          print_gpt_partitions partitions
       | Error msg ->
-          Printf.printf "Failed to read GPT from %s: %s\n" gpt msg;
+          Printf.printf "Failed to read GPT header from %s: %s\n" gpt msg;
           exit 1)
     gpts
 
