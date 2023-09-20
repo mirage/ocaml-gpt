7e56c7e789bfdf41d2249990e2f2f1403473534c
Reynir Björnsson <reynir@reynir.dk>
2023-09-20 10:40:04 +0200

    Refactor and fix first_usable_lba
    Use Result let-syntax

--- lib/gpt.ml
+++ lib/gpt.ml
@@ -13,7 +13,7 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
-let ( >>= ) = Result.bind
+let ( let* ) = Result.bind
 let sizeof = 128
 
 let guid_len = 16
@@ -30,9 +30,11 @@ module Partition = struct
 
   let make ?(name =  String.make 72 '\000') ~type_guid ~attributes starting_lba ending_lba =
     let partition_guid = Uuidm.v4_gen (Random.State.make_self_init ()) () in
-    (if String.length name > 72 then
-       Error (Printf.sprintf "Name length %d should be less than or equal to 72\n" (String.length name))
-     else Ok ()) >>= fun () ->
+    let* () =
+      if String.length name > 72 then
+        Error (Printf.sprintf "Name length %d should be less than or equal to 72\n" (String.length name))
+      else Ok ()
+    in
     Ok
       {
         type_guid;
@@ -54,17 +56,18 @@ module Partition = struct
   let name_offset = 56
 
   let unmarshal buf =
-    (if Cstruct.length buf < sizeof then
-     Error
-       (Printf.sprintf "Partition entry too small: %d < %d" (Cstruct.length buf)
-          sizeof)
-    else Ok ())
-    >>= fun () ->
+    let* () =
+      if Cstruct.length buf < sizeof then
+        Error
+          (Printf.sprintf "Partition entry too small: %d < %d" (Cstruct.length buf)
+             sizeof)
+      else Ok ()
+    in
     let type_guid_bytes =
       Cstruct.sub buf type_guid_offset guid_len
       |> Cstruct.to_string
     in
-    let type_guid =
+    let* type_guid =
       match Uuidm.of_mixed_endian_bytes type_guid_bytes with
       | Some guid -> Ok guid
       | None ->
@@ -72,12 +75,11 @@ module Partition = struct
             (Printf.sprintf "Failed to parse the partition type guid; got '%s'"
                type_guid_bytes)
     in
-    type_guid >>= fun type_guid ->
     let partition_guid_bytes =
       Cstruct.sub buf partition_guid_offset guid_len
       |> Cstruct.to_string
     in
-    let partition_guid =
+    let* partition_guid =
       match Uuidm.of_mixed_endian_bytes partition_guid_bytes with
       | Some guid -> Ok guid
       | None ->
@@ -86,7 +88,6 @@ module Partition = struct
                "Failed to parse the unique partition guid; got '%s'"
                type_guid_bytes)
     in
-    partition_guid >>= fun partition_guid ->
     let starting_lba = Cstruct.LE.get_uint64 buf starting_lba_offset in
     let ending_lba = Cstruct.LE.get_uint64 buf ending_lba_offset in
     let attributes = Cstruct.LE.get_uint64 buf attributes_offset in
@@ -179,66 +180,72 @@ let calculate_partition_crc32 partitions =
 
 let table_sectors_required num_partition_entries sector_size =
   (((num_partition_entries * sizeof) + sector_size - 1) /sector_size)
+
 let make ?(disk_guid) ~disk_size ~sector_size partitions =
   let num_partition_entries = 128 in
   let num_actual_partition_entries = List.length partitions in
-  if num_actual_partition_entries > num_partition_entries then
-    Error
-      ((Printf.sprintf "Number of partitions %d exceeds required number %d\n%!")
-        num_actual_partition_entries num_partition_entries)
-  else
-    let partition_table_sectors = table_sectors_required num_partition_entries sector_size in
-    let partitions =
-      List.sort
-        (fun p1 p2 ->
-          Int64.unsigned_compare p1.Partition.starting_lba
-            p2.Partition.starting_lba)
-        partitions
-    in
-    (* Check for overlapping partions *)
+  let* () =
+    if num_actual_partition_entries > num_partition_entries then
+      Error
+        ((Printf.sprintf "Number of partitions %d exceeds required number %d\n%!")
+           num_actual_partition_entries num_partition_entries)
+    else
+      Ok ()
+  in
+  let partitions =
+    List.sort
+      (fun p1 p2 ->
+         Int64.unsigned_compare p1.Partition.starting_lba
+           p2.Partition.starting_lba)
+      partitions
+  in
+  (* Check for overlapping partions *)
+  let* _last_partition_lba =
     List.fold_left
       (fun r p ->
-        r >>= fun offset ->
-        if Int64.unsigned_compare offset p.Partition.starting_lba <= 0 then
-          Ok (Int64.add p.Partition.starting_lba p.Partition.ending_lba)
-        else Error (Printf.sprintf "Partitions overlap"))
+         let* offset = r in
+         if Int64.unsigned_compare offset p.Partition.starting_lba <= 0 then
+           Ok (Int64.add p.Partition.starting_lba p.Partition.ending_lba)
+         else Error (Printf.sprintf "Partitions overlap"))
       (Ok 1L) partitions
-    >>= fun (_ : int64) ->
-    let current_lba = 1L in
-    let backup_lba = Int64.sub disk_size 1L in
-    let last_usable_lba = Int64.sub backup_lba 1L in
-    let partition_entry_lba = 2L in
-    let first_usable_lba = Int64.add (Int64.sub ((Int64.add (Int64.of_int partition_table_sectors) partition_entry_lba)) 1L) 1L in
-    let disk_guid = Option.value disk_guid ~default:(Uuidm.v4_gen (Random.State.make_self_init ()) ()) in
-    let partition_size = Int32.of_int Partition.sizeof in
-    let header_size = Int32.of_int sizeof in
-    let revision = 0x010000l in
-    let reserved = 0l in
-    let num_partition_entries = Int32.of_int num_partition_entries in
-    let partitions_crc32 =
-      Optint.to_int32 (calculate_partition_crc32 partitions)
-    in
-    let header =
-      {
-        revision;
-        header_size;
-        header_crc32 = 0l;
-        reserved;
-        current_lba;
-        backup_lba;
-        first_usable_lba;
-        last_usable_lba;
-        disk_guid;
-        partition_entry_lba;
-        num_partition_entries;
-        partitions;
-        partition_size;
-        partitions_crc32;
-      }
-    in
-    let header_crc32 = Optint.to_int32 (calculate_header_crc32 header) in
-    Ok { header with header_crc32 }
-
+  in
+  let current_lba = 1L in
+  let backup_lba = Int64.sub disk_size 1L in
+  let last_usable_lba = Int64.sub backup_lba 1L in
+  let partition_entry_lba = 2L in
+  let first_usable_lba =
+    let partition_table_sectors = table_sectors_required num_partition_entries sector_size in
+    Int64.(add partition_entry_lba (of_int partition_table_sectors))
+  in
+  let disk_guid = Option.value disk_guid ~default:(Uuidm.v4_gen (Random.State.make_self_init ()) ()) in
+  let partition_size = Int32.of_int Partition.sizeof in
+  let header_size = Int32.of_int sizeof in
+  let revision = 0x010000l in
+  let reserved = 0l in
+  let num_partition_entries = Int32.of_int num_partition_entries in
+  let partitions_crc32 =
+    Optint.to_int32 (calculate_partition_crc32 partitions)
+  in
+  let header =
+    {
+      revision;
+      header_size;
+      header_crc32 = 0l;
+      reserved;
+      current_lba;
+      backup_lba;
+      first_usable_lba;
+      last_usable_lba;
+      disk_guid;
+      partition_entry_lba;
+      num_partition_entries;
+      partitions;
+      partition_size;
+      partitions_crc32;
+    }
+  in
+  let header_crc32 = Optint.to_int32 (calculate_header_crc32 header) in
+  Ok { header with header_crc32 }
 
 let signature_offset = 0
 let signature_len = 8
@@ -256,93 +263,99 @@ let num_partition_entries_offset = 80
 let partition_size_offset = 84
 let partitions_crc32_offset = 88
 
-let unmarshal buf ~sector_size = (* Read from sector 2 to sector 34. Sector 1 is protective MBR*)
-  if Cstruct.length buf < sizeof then
-    Error (Printf.sprintf "GPT too small: %d < %d" (Cstruct.length buf) sizeof)
-  else
-    let signature =
-      Cstruct.sub buf signature_offset signature_len |> Cstruct.to_string
-    in
+let unmarshal buf ~sector_size =
+  let* () =
+    if Cstruct.length buf < sizeof then
+      Error (Printf.sprintf "GPT too small: %d < %d" (Cstruct.length buf) sizeof)
+    else
+      Ok ()
+  in
+  let signature =
+    Cstruct.sub buf signature_offset signature_len |> Cstruct.to_string
+  in
+  let* () =
     match signature with
-    | "EFI PART" ->
-        let revision = Cstruct.LE.get_uint32 buf revision_offset in
-        if revision = 0x010000l then
-          let header_size = Cstruct.LE.get_uint32 buf header_size_offset in
-          let header_crc32 = Cstruct.LE.get_uint32 buf header_crc32_offset in
-          let reserved = Cstruct.LE.get_uint32 buf reserved_offset in
-          let current_lba = Cstruct.LE.get_uint64 buf current_lba_offset in
-          let backup_lba = Cstruct.LE.get_uint64 buf backup_lba_offset in
-          let first_usable_lba =
-            Cstruct.LE.get_uint64 buf first_usable_lba_offset
-          in
-          let last_usable_lba =
-            Cstruct.LE.get_uint64 buf last_usable_lba_offset
-          in
-          let disk_guid_bytes =
-            Cstruct.sub buf disk_guid_offset guid_len
-            |> Cstruct.to_string
-          in
-          let disk_guid =
-            match Uuidm.of_mixed_endian_bytes disk_guid_bytes with
-            | Some guid -> Ok guid
-            | None ->
-                Error
-                  (Printf.sprintf "Failed to parse disk_guid; got '%s'"
-                     disk_guid_bytes)
-          in
-          disk_guid >>= fun disk_guid ->
-          let partition_entry_lba =
-            Cstruct.LE.get_uint64 buf partition_entry_lba_offset
-          in
-          let num_partition_entries =
-            Cstruct.LE.get_uint32 buf num_partition_entries_offset
-          in
-          let partitions_crc32 =
-            Cstruct.LE.get_uint32 buf partitions_crc32_offset
-          in
-          let partition_size =
-            Cstruct.LE.get_uint32 buf partition_size_offset
-          in
-          let partition_buf = Cstruct.create ((Int32.to_int partition_size) * (Int32.to_int num_partition_entries)) in
-          Cstruct.blit buf sector_size partition_buf 0 (Cstruct.length partition_buf);
-          let partition_entries = ref [] in
-          for i = 0 to Int32.to_int num_partition_entries - 1 do
-            let partition_entry_offset = i * Int32.to_int partition_size in
-            let partition_entry_buf =
-              Cstruct.sub
-                partition_buf
-                partition_entry_offset
-                (Int32.to_int partition_size)
-            in
-            match Partition.unmarshal partition_entry_buf with
-            | Ok entry -> partition_entries := entry :: !partition_entries
-            | Error e -> Printf.printf "An error occurred: %s\n" e
-          done;
-          let partitions = List.rev !partition_entries in
-          Ok
-            {
-              revision;
-              header_size;
-              header_crc32;
-              reserved;
-              current_lba;
-              backup_lba;
-              first_usable_lba;
-              last_usable_lba;
-              disk_guid;
-              partition_entry_lba;
-              num_partition_entries;
-              partitions;
-              partition_size;
-              partitions_crc32;
-            }
-        else
-          Error
-            (Printf.sprintf "Unknown revision; expected 0x10000, got 0x%08lx"
-               revision)
+    | "EFI PART" -> Ok ()
     | x ->
-        Error
-          (Printf.sprintf "Signature not found; expected 'EFI PART', got '%s'" x)
+      Error
+        (Printf.sprintf "Signature not found; expected 'EFI PART', got '%s'" x)
+  in
+  let revision = Cstruct.LE.get_uint32 buf revision_offset in
+  let* () =
+    if revision = 0x010000l then Ok ()
+    else
+      Error
+        (Printf.sprintf "Unknown revision; expected 0x10000, got 0x%08lx"
+           revision)
+  in
+  let header_size = Cstruct.LE.get_uint32 buf header_size_offset in
+  let header_crc32 = Cstruct.LE.get_uint32 buf header_crc32_offset in
+  let reserved = Cstruct.LE.get_uint32 buf reserved_offset in
+  let current_lba = Cstruct.LE.get_uint64 buf current_lba_offset in
+  let backup_lba = Cstruct.LE.get_uint64 buf backup_lba_offset in
+  let first_usable_lba =
+    Cstruct.LE.get_uint64 buf first_usable_lba_offset
+  in
+  let last_usable_lba =
+    Cstruct.LE.get_uint64 buf last_usable_lba_offset
+  in
+  let disk_guid_bytes =
+    Cstruct.sub buf disk_guid_offset guid_len
+    |> Cstruct.to_string
+  in
+  let* disk_guid =
+    match Uuidm.of_mixed_endian_bytes disk_guid_bytes with
+    | Some guid -> Ok guid
+    | None ->
+      Error
+        (Printf.sprintf "Failed to parse disk_guid; got '%s'"
+           disk_guid_bytes)
+  in
+  let partition_entry_lba =
+    Cstruct.LE.get_uint64 buf partition_entry_lba_offset
+  in
+  let num_partition_entries =
+    Cstruct.LE.get_uint32 buf num_partition_entries_offset
+  in
+  let partitions_crc32 =
+    Cstruct.LE.get_uint32 buf partitions_crc32_offset
+  in
+  let partition_size =
+    Cstruct.LE.get_uint32 buf partition_size_offset
+  in
+  let partition_buf = Cstruct.create ((Int32.to_int partition_size) * (Int32.to_int num_partition_entries)) in
+  Cstruct.blit buf sector_size partition_buf 0 (Cstruct.length partition_buf);
+  let partition_entries = ref [] in
+  for i = 0 to Int32.to_int num_partition_entries - 1 do
+    let partition_entry_offset = i * Int32.to_int partition_size in
+    let partition_entry_buf =
+      Cstruct.sub
+        partition_buf
+        partition_entry_offset
+        (Int32.to_int partition_size)
+    in
+    match Partition.unmarshal partition_entry_buf with
+    | Ok entry -> partition_entries := entry :: !partition_entries
+    | Error e -> Printf.printf "An error occurred: %s\n" e
+  done;
+  let partitions = List.rev !partition_entries in
+  Ok
+    {
+      revision;
+      header_size;
+      header_crc32;
+      reserved;
+      current_lba;
+      backup_lba;
+      first_usable_lba;
+      last_usable_lba;
+      disk_guid;
+      partition_entry_lba;
+      num_partition_entries;
+      partitions;
+      partition_size;
+      partitions_crc32;
+    }
 
 let marshal (buf : Cstruct.t) t =
   Cstruct.blit_from_string signature 0 buf signature_offset revision_offset;
